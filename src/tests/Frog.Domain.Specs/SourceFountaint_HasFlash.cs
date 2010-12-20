8f5990b022a7959752c38b1b46cb0f08171c12bf
Petar Shomov <petar@spretturmarimo.is>
2010-12-20 23:47:20 +0000

    some more renaming to move to the pipeline and sie notion

--- src/tests/Frog.Domain.Specs/SourceFountaint_HasFlash.cs
+++ src/tests/Frog.Domain.Specs/SourceFountaint_HasFlash.cs
@@ -4,43 +4,43 @@ using Rhino.Mocks;
 
 namespace Frog.Domain.Specs
 {
-    public class SourceFountaint_First_Run : SourceFountaintSpec
+    public class Repository_First_Run : RepositorySpec
     {
-        Establish context = () => fountain = new Repository(repo, bus);
-        Because of = () => fountain.CheckForUpdates();
+        Establish context = () => repository = new Repository(repo, bus);
+        Because of = () => repository.CheckForUpdates();
         It should_check_source_repo = () => repo.AssertWasCalled(x => x.LatestRevisionNumber);
         It should_send_message = () => bus.AssertWasCalled(x => x.PostTopic("src_new_revision"));
     }
 
-    public class SourceFountaint_Later_Run : SourceFountaintSpec
+    public class Repository_Later_Run : RepositorySpec
     {
         private Establish context = () =>
                                         {
-                                            fountain = new Repository(repo, bus);
+                                            repository = new Repository(repo, bus);
                                             repo.Stub(x => x.LatestRevisionNumber).Return(1).Return(2);
-                                            fountain.CheckForUpdates();
+                                            repository.CheckForUpdates();
                                         };
-        Because of = () => fountain.CheckForUpdates();
+        Because of = () => repository.CheckForUpdates();
         It should_check_source_repo_twice = () => repo.AssertWasCalled(x => x.LatestRevisionNumber, options => options.Repeat.Twice());
         It should_send_message = () => bus.AssertWasCalled(x => x.PostTopic("src_new_revision"));
     }
 
-    public class SourceFountaint_No_Change_In_Src_Revision_Run : SourceFountaintSpec
+    public class Repository_No_Change_In_Src_Revision_Run : RepositorySpec
     {
         private Establish context = () =>
                                         {
-                                            fountain = new Repository(repo, bus);
+                                            repository = new Repository(repo, bus);
                                             repo.Stub(x => x.LatestRevisionNumber).Return(1).Repeat.Any();
-                                            fountain.CheckForUpdates();
+                                            repository.CheckForUpdates();
                                         };
 
-        Because of = () => fountain.CheckForUpdates();
+        Because of = () => repository.CheckForUpdates();
         It should_not_send_message = () => bus.AssertWasCalled(x => x.PostTopic("src_new_revision"), options => options.Repeat.Once());
     }
 
-    public class SourceFountaintSpec
+    public class RepositorySpec
     {
-        public static Repository fountain;
+        public static Repository repository;
         public static MessageBus bus;
         public static SourceRepo repo;
 
