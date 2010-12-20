8c47c00726700e051b705ebc33996fc12ab5eb6a
Petar Shomov <petar@spretturmarimo.is>
2010-12-20 01:57:20 +0000

    moving the snapshot taking and matching to xray where it belongs, will experiment there with that, will focus on frog here

--- src/tests/Frog.System.Specs/Attempting_a_system_test_on_my_own.cs
+++ src/tests/Frog.System.Specs/Attempting_a_system_test_on_my_own.cs
@@ -8,34 +8,6 @@ using xray;
 namespace Frog.System.Specs
 {
 
-    internal class ValueMatchingProbe<T> : Probe
-    {
-        private readonly IMatcher<T> matcher;
-        private readonly Func<T> Snapshot;
-
-        public ValueMatchingProbe(Func<T> snapshot, IMatcher<T> matcher)
-        {
-            Snapshot = snapshot;
-            this.matcher = matcher;
-        }
-
-        #region Probe Members
-
-        public bool probeAndMatch()
-        {
-            return matcher.Matches(Snapshot());
-        }
-
-        #endregion
-    }
-
-    public class Spec
-    {
-        public static IMatcher<T> Has<T, V>(NHamcrest.Func<T, V> checker, IMatcher<V> matcher)
-        {
-            return new NestedMatcher<T, V>(checker, matcher);
-        }
-    }
 
     public class Attempting_a_system_test_on_my_own
     {
@@ -46,10 +18,6 @@ namespace Frog.System.Specs
         private Establish context = () => { _theDriver = new TestDriver(); };
 
         private Because of = () => _theDriver.SourceFontain.HasSplash();
-
-        private It should_start_the_pipeline =
-            () =>
-            Prober.check(ValueProbe(() => _theDriver.Stream1, has<FakeStream, bool>(x => x.GotWater, Is.EqualTo(true))));
 
         private It should_do_one_more = () => Test();
 
@@ -62,15 +30,6 @@ namespace Frog.System.Specs
         }
 
 
-        private static Probe ValueProbe<T>(Func<T> query, IMatcher<T> matcher)
-        {
-            return new ValueMatchingProbe<T>(query, matcher);
-        }
-
-        private static IMatcher<T> has<T, V>(NHamcrest.Func<T, V> query, IMatcher<V> matcher)
-        {
-            return Spec.Has(query, matcher);
-        }
     }
 
 }
