e3aa06e0fa77158c9537083ebd79069b29401beb
Petar Shomov <pshomov@gmail.com>
2010-12-30 16:56:24 +0000

    reformatted the code

--- src/tests/Frog.Domain.Specs/PipelineProcessSourceDrop.cs
+++ src/tests/Frog.Domain.Specs/PipelineProcessSourceDrop.cs
@@ -6,12 +6,13 @@ namespace Frog.Domain.Specs
     public class Pipeline_processes_sourceDrop : PipelineProcessSourceDropBase
     {
         Establish context = () =>
-                                        {
-                                            _task1 = MockRepository.GenerateMock<ExecTask>("");
-                                            _task1.Expect(task => task.Perform(null)).IgnoreArguments().Return(
-                                                new ExecTaskResult(ExecTask.ExecutionStatus.Success, 0));
-                                            _pipeline = new PipelineOfTasks(_task1);
-                                        };
+                                {
+                                    _task1 = MockRepository.GenerateMock<ExecTask>("");
+                                    _task1.Expect(task => task.Perform(null)).IgnoreArguments().Return(
+                                        new ExecTaskResult(ExecTask.ExecutionStatus.Success, 0));
+                                    _pipeline = new PipelineOfTasks(_task1);
+                                };
+
         Because of = () => _pipeline.Process(SourceDrop);
         It should_start_first_task = () => _task1.AssertWasCalled(task => task.Perform(SourceDrop));
     }
@@ -19,14 +20,16 @@ namespace Frog.Domain.Specs
     public class Pipeline_process_sourceDrop_after_first_task_succeeds : PipelineProcessSourceDropBase
     {
         Establish context = () =>
-                                        {
-                                            _task1 = MockRepository.GenerateStub<ExecTask>("");
-                                            _task1.Expect(task => task.Perform(null)).IgnoreArguments().Return(new ExecTaskResult(ExecTask.ExecutionStatus.Success, 0));
-                                            _task2 = MockRepository.GenerateMock<ExecTask>("");
-                                            _task2.Expect(task => task.Perform(null)).IgnoreArguments().Return(
-                                                new ExecTaskResult(ExecTask.ExecutionStatus.Success, 0));
-                                            _pipeline = new PipelineOfTasks(_task1, _task2);
-                                        };
+                                {
+                                    _task1 = MockRepository.GenerateStub<ExecTask>("");
+                                    _task1.Expect(task => task.Perform(null)).IgnoreArguments().Return(
+                                        new ExecTaskResult(ExecTask.ExecutionStatus.Success, 0));
+                                    _task2 = MockRepository.GenerateMock<ExecTask>("");
+                                    _task2.Expect(task => task.Perform(null)).IgnoreArguments().Return(
+                                        new ExecTaskResult(ExecTask.ExecutionStatus.Success, 0));
+                                    _pipeline = new PipelineOfTasks(_task1, _task2);
+                                };
+
         Because of = () => _pipeline.Process(SourceDrop);
         It should_perform_later_tasks = () => _task2.AssertWasCalled(task => task.Perform(SourceDrop));
     }
@@ -34,12 +37,14 @@ namespace Frog.Domain.Specs
     public class Pipeline_process_sourceDrop_task_failure : PipelineProcessSourceDropBase
     {
         Establish context = () =>
-                                        {
-                                            _task1 = MockRepository.GenerateStub<ExecTask>("");
-                                            _task1.Expect(task => task.Perform(null)).IgnoreArguments().Return(new ExecTaskResult(ExecTask.ExecutionStatus.Failure, 2));
-                                            _task2 = MockRepository.GenerateMock<ExecTask>("");
-                                            _pipeline = new PipelineOfTasks(_task1, _task2);
-                                        };
+                                {
+                                    _task1 = MockRepository.GenerateStub<ExecTask>("");
+                                    _task1.Expect(task => task.Perform(null)).IgnoreArguments().Return(
+                                        new ExecTaskResult(ExecTask.ExecutionStatus.Failure, 2));
+                                    _task2 = MockRepository.GenerateMock<ExecTask>("");
+                                    _pipeline = new PipelineOfTasks(_task1, _task2);
+                                };
+
         Because of = () => _pipeline.Process(SourceDrop);
         It should_not_perform_later_tasks = () => _task2.AssertWasNotCalled(task => task.Perform(SourceDrop));
     }
