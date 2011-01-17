9a6cdf5f2301f6c32d2503b82c3f655e98e7ed0e
Petar Shomov <pshomov@gmail.com>
2011-01-17 08:53:22 +0000

    cleaning up a bit

--- src/tests/Frog.Specs.Support/git_support_scripts/git_create_dummy_repo.rb
+++ src/tests/Frog.Specs.Support/git_support_scripts/git_create_dummy_repo.rb
@@ -10,14 +10,3 @@ system 'git add test.txt'
 exit $? if $? != 0
 system 'git commit -m "commit number one"'
 exit $? if $? != 0
-
-#
-#%~d1
-#cd %1
-#if EXIST %2 RMDIR /S/Q %2
-#mkdir %2
-#cd %2
-#cmd.exe /c git init .
-#echo line 1 > test.txt
-#cmd.exe /c git add test.txt
-#cmd.exe /c git commit -m "commit number one"
