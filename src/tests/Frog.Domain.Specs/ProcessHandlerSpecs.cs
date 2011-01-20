7fb6569a1bf4bbbb2c3943176dedf5451137ca68
Petar Shomov <petar@spretturmarimo.is>
2011-01-20 00:40:01 +0000

    from specs back to unit tests

--- src/tests/Frog.Domain.Specs/ProcessHandlerSpecs.cs
+++ src/tests/Frog.Domain.Specs/ProcessHandlerSpecs.cs
@@ -4,55 +4,33 @@ using NUnit.Framework;
 
 namespace Frog.Domain.Specs
 {
+
     [TestFixture]
-    public class ProcessHandlerSpecs : BDD
+    public class ProcessHandlerIOCaptureSpecs
     {
-        StringBuilder err;
-        ProcessWrapper pw;
 
-        public override void Given()
+        [Test]
+        public void should_capture_std_output()
         {
-            err = new StringBuilder();
-            pw = new ProcessWrapper("cmd.exe", "/c asdfasdfddd@!#!11fasfasdf.asdfasdf");
-            pw.OnErrorOutput += output => err.Append(output);
-        }
+            var std = new StringBuilder();
+            var pw = new ProcessWrapper("ruby", "-e 'puts \"ff\"'");
+            pw.OnStdOutput += output => std.Append(output);
 
-        public override void When()
-        {
             pw.Execute();
             pw.WaitForProcess();
+
+            Assert.That(std.ToString(), Is.Not.Empty);
         }
 
         [Test]
         public void should_capture_std_error_output()
         {
-            Assert.That(err.ToString(), Is.Not.Empty);
-        }
-    }
-
-    [TestFixture]
-    public class ProcessHandlerStandardOutCaptureSpecs : BDD
-    {
-        StringBuilder std;
-        ProcessWrapper pw;
-
-        public override void Given()
-        {
-            std = new StringBuilder();
-            pw = new ProcessWrapper("cmd.exe", "/c dir");
-            pw.OnStdOutput += output => std.Append(output);
-        }
-
-        public override void When()
-        {
+            var err = new StringBuilder();
+            var pw = new ProcessWrapper("ruby", "-e asdfasdfddd@!#!11fasfasdf.asdfasdf");
+            pw.OnErrorOutput += output => err.Append(output);
             pw.Execute();
             pw.WaitForProcess();
-        }
-
-        [Test]
-        public void should_capture_std_output()
-        {
-            Assert.That(std.ToString(), Is.Not.Empty);
+            Assert.That(err.ToString(), Is.Not.Empty);
         }
     }
 }
