b1d6a984b114360c536daedf2b6ef3fa3c52782a
Petar Shomov <pshomov@gmail.com>
2011-01-23 01:24:37 +0000

    adding task factory + a small moving around

---
+++ src/app/Frog.Domain/TaskDetector.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Frog.Domain.Specs;
+
+namespace Frog.Domain
+{
+    public interface TaskDetector
+    {
+        IList<ExecTask> Detect();
+    }
+}
---
+++ src/app/Frog.Domain/TaskFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Frog.Domain.Specs;
+
+namespace Frog.Domain
+{
+    public class TaskFactory
+    {
+        readonly TaskDetector[] taskDetectors;
+
+        public TaskFactory(params TaskDetector[] taskDetectors)
+        {
+            this.taskDetectors = taskDetectors;
+        }
+
+        public IList<ExecTask> Generate()
+        {
+            return taskDetectors[0].Detect();
+        }
+    }
+}
