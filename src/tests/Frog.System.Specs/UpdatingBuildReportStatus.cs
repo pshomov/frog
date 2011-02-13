1cd5a4d119c6ca9476a7acbf98ed7e934147aff2
Petar Shomov <pshomov@gmail.com>
2011-02-13 15:53:19 +0000

    removing wrong specs

--- src/tests/Frog.System.Specs/UpdatingBuildReportStatus.cs
+++ src/tests/Frog.System.Specs/UpdatingBuildReportStatus.cs
@@ -1,17 +1,14 @@
-using System.Collections.Generic;
 using System.Threading;
 using Frog.Domain;
 using Frog.Domain.CustomTasks;
 using Frog.Domain.Specs;
 using Frog.Domain.TaskDetection;
-using Frog.Domain.TaskSources;
-using Frog.Domain.UI;
 using Frog.Specs.Support;
 using Frog.Support;
 using NSubstitute;
 using NUnit.Framework;
-using SimpleCQRS;
 using xray;
+using Has = NHamcrest.Core.Has;
 using Is = NHamcrest.Core.Is;
 
 namespace Frog.System.Specs
@@ -29,7 +26,8 @@ namespace Frog.System.Specs
             system = SystemDriver.GetCleanSystem();
             repo = RepositoryDriver.GetNewRepository();
             var execTaskGenerator = Substitute.For<IExecTaskGenerator>();
-            execTaskGenerator.GimeTasks(Arg.Any<ITask>()).Returns(As.List(new ExecTask(@"ruby", @"-e 'exit 2'", "task_name")));
+            execTaskGenerator.GimeTasks(Arg.Any<ITask>()).Returns(
+                As.List(new ExecTask(@"ruby", @"-e 'exit 2'", "task_name")));
             pipeline = new PipelineOfTasks(system.Bus, new FixedTasksDispenser(new MSBuildTaskDescriptions("asdasd")),
                                            execTaskGenerator);
             system.MonitorRepository(repo.Url);
@@ -46,24 +44,12 @@ namespace Frog.System.Specs
         {
             var prober = new PollingProber(3000, 100);
             Assert.True(prober.check(Take.Snapshot(() => system.GetEventsSnapshot())
-                                         .Has(x => x, NHamcrest.Core.Has.Item(Is.InstanceOf(typeof(BuildStarted))))
-                                         .Has(x => x, NHamcrest.Core.Has.Item(Is.InstanceOf(typeof(BuildUpdated))))
-                                         .Has(x => x, NHamcrest.Core.Has.Item(Is.InstanceOf(typeof(BuildUpdated))))
-                                         .Has(x => x, NHamcrest.Core.Has.Item(Is.InstanceOf(typeof(BuildEnded))))
+                                         .Has(x => x, Has.Item(Is.InstanceOf(typeof (BuildStarted))))
+                                         .Has(x => x, Has.Item(Is.InstanceOf(typeof (BuildUpdated))))
+                                         .Has(x => x, Has.Item(Is.InstanceOf(typeof (BuildUpdated))))
+                                         .Has(x => x, Has.Item(Is.InstanceOf(typeof (BuildEnded))))
                             ));
         }
-
-//        [Test]
-//        public void should_have_build_result_in_build_complete_event()
-//        {
-//            var prober = new PollingProber(3000, 100);
-//            Assert.True(prober.check(Take.Snapshot(() => system.CurrentReport).
-//                                         Has(x => x.Current, Is.EqualTo(PipelineStatusView.BuildStatus.Status.PipelineComplete))
-//                                         .
-//                                         Has(x => x.Completion,
-//                                             Is.EqualTo(PipelineStatusView.BuildStatus.TaskExit.Error))
-//                            ));
-//        }
 
         protected override void GivenCleanup()
         {
