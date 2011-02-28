44cb5ab754230edb3202e8f00a5b615f122b9299
Petar Shomov <pshomov@gmail.com>
2011-02-28 09:05:44 +0000

    fixing the console test runner

--- run_tests.bat
+++ run_tests.bat
@@ -1,7 +1,8 @@
-libs\NUnit\nunit-console.exe src\tests\Frog.Domain.Specs\bin\Debug\Frog.Domain.Tests.dll
+libs\NUnit\nunit-console.exe src\tests\Frog.Domain.Specs\bin\Debug\Frog.Domain.Test.dll
 @if %errorlevel% neq 0 exit /b %errorlevel%
-libs\mspec\mspec.exe src\tests\Frog.Domain.Specs\bin\Debug\Frog.Domain.Tests.dll
+libs\mspec\mspec.exe src\tests\Frog.Domain.Specs\bin\Debug\Frog.Domain.Test.dll
 @if %errorlevel% neq 0 exit /b %errorlevel%
-libs\NUnit\nunit-console.exe src\tests\Frog.System.Specs\bin\Debug\Frog.System.Tests.dll
+libs\NUnit\nunit-console.exe src\tests\Frog.System.Specs\bin\Debug\Frog.System.Test.dll
+echo "Error level is " %errorlevel%
 @if %errorlevel% neq 0 exit /b %errorlevel%
 @echo "Build Runz!"
