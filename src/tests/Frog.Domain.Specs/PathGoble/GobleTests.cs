e80622d63f7990ea9ad89e165ec5762eb72dcc85
Petar Shomov <pshomov@gmail.com>
2011-03-15 23:45:32 +0000

    removing the hardcoded file hierarchy and replacing it wth the cool file genesis API

--- src/tests/Frog.Domain.Specs/PathGoble/GobleTests.cs
+++ src/tests/Frog.Domain.Specs/PathGoble/GobleTests.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using Frog.Specs.Support;
 using Frog.Support;
 using NUnit.Framework;
 
@@ -7,14 +9,38 @@ namespace Frog.Domain.Specs.PathGoble
     [TestFixture]
     public class GobleTests
     {
+        List<string> list;
+        PathFinder pathFinder;
+        string workPlace;
+
+        [SetUp]
+        public void Setup()
+        {
+            list = new List<string>();
+            pathFinder = new PathFinder();
+
+            workPlace = Path.Combine(GitTestSupport.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(workPlace);
+            var genesis = new FileGenesis(workPlace);
+            genesis.Folder("TestFixtures")
+                .File("test.txt", "")
+                .Folder("Goble")
+                    .File("l0f1.txt", "")
+                    .File("l0f2.txt", "")
+                    .Folder("level1")
+                        .File("l1f1.txt", "")
+                        .File("l1f2.txt", "")
+                        .Folder("level2")
+                            .File("l2f1.txt", "")
+                            .File("l2f2.txt", "");
+
+
+        }
+
         [Test]
         public void should_find_file_down_a_folder_hieararchy()
         {
-            var list = new List<string>();
-            var pathFinder =
-                new PathFinder();
-
-            pathFinder.apply(list.Add, "l1f1.txt", "TestFixtures");
+            pathFinder.apply(s => list.Add(s.Remove(0, workPlace.Length+1)), "l1f1.txt", Path.Combine(workPlace, "TestFixtures"));
 
             Assert.That(list.Count, Is.EqualTo(1));
             Assert.That(list[0],
@@ -24,11 +50,7 @@ namespace Frog.Domain.Specs.PathGoble
         [Test]
         public void should_find_multiple_files_at_the_same_level_down_the_hierarchy()
         {
-            var list = new List<string>();
-            var pathFinder =
-                new PathFinder();
-
-            pathFinder.apply(list.Add, "l2*.txt", "TestFixtures");
+            pathFinder.apply(s => list.Add(s.Remove(0, workPlace.Length + 1)), "l2*.txt", Path.Combine(workPlace, "TestFixtures"));
 
             Assert.That(list.Count, Is.EqualTo(2));
             Assert.That(list[0],
@@ -40,11 +62,7 @@ namespace Frog.Domain.Specs.PathGoble
         [Test]
         public void should_find_multiple_files_at_different_levels_down_the_hierarchy()
         {
-            var list = new List<string>();
-            var pathFinder =
-                new PathFinder();
-
-            pathFinder.apply(list.Add, "l?f*.txt", "TestFixtures");
+            pathFinder.apply(s => list.Add(s.Remove(0, workPlace.Length + 1)), "l?f*.txt", Path.Combine(workPlace, "TestFixtures"));
 
             Assert.That(list.Count, Is.EqualTo(6));
             Assert.That(list[0],
