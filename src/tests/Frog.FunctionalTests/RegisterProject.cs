c0f2bbf275ce5fae665bf228d42a8181be4d2376
Petar Shomov <pshomov@gmail.com>
2011-03-21 22:43:36 +0000

    refactoring a bit

--- src/tests/Frog.FunctionalTests/RegisterProject.cs
+++ src/tests/Frog.FunctionalTests/RegisterProject.cs
@@ -5,7 +5,6 @@ using Frog.Domain.Specs;
 using Frog.Specs.Support;
 using Frog.Support;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
@@ -16,36 +15,30 @@ namespace Frog.FunctionalTests
     public class RegisterProject
     {
         RemoteWebDriver driver;
+        FirefoxDriver driver2;
         string baseUrl;
         string basePath;
-		FirefoxDriver driver2;
 
         [Test]
         public void should_register_project_successfully()
         {
-            basePath = GetMeAWorkingFolder();
+            basePath = OSHelpers.GetMeAWorkingFolder();
             string repo = GitTestSupport.CreateDummyRepo(basePath, "testrepo");
-			
-            string changeset = GetMeAWorkingFolder();
-            var gen = new FileGenesis(changeset);
-			gen.File("build.sln", "invalid");
-			GitTestSupport.CommitChangeFiles(repo, changeset);
-			
+
+            var gen = new FileGenesis();
+            gen.File("build.sln", "invalid");
+            GitTestSupport.CommitChangeFiles(repo, gen.Root);
+
             driver.Navigate().GoToUrl(U("Content/register.html"));
             driver.FindElement(By.Id("url")).SendKeys(repo);
             driver.FindElementById("reg_button").Click();
             WithRetries(() => driver.FindElementById("newly_registered")).Click();
             driver2 = new FirefoxDriver();
             driver2.Navigate().GoToUrl(U("system/check"));
-            WithRetries(() => Assert.That(driver.FindElement(By.CssSelector("#status")).Text, Is.EqualTo("Build complete")));	
+            WithRetries(
+                () => Assert.That(driver.FindElement(By.CssSelector("#status")).Text, Is.EqualTo("Build complete")));
         }
 
-        string GetMeAWorkingFolder()
-        {
-            var changeset = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-            Directory.CreateDirectory(changeset);
-            return changeset;
-        }
 
         static T WithRetries<T>(Func<T> query, int retries = 100)
         {
@@ -66,7 +59,7 @@ namespace Frog.FunctionalTests
             }
         }
 
-        static void WithRetries(Action action,  int retries = 100)
+        static void WithRetries(Action action, int retries = 100)
         {
             while (true)
             {
@@ -75,7 +68,7 @@ namespace Frog.FunctionalTests
                     action();
                     return;
                 }
-                catch(AssertionException)
+                catch (AssertionException)
                 {
                     if (retries > 0)
                     {
