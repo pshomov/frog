a4770e6eb9112ef1121d4bb98f975dafdcf2bdb0
Petar Shomov <pshomov@gmail.com>
2011-03-22 00:24:21 +0000

    switched over to using SpecFlow for describing the acceptance specs

---
+++ src/tests/Frog.Specs.Support/AssertionHelpers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Frog.Specs.Support
+{
+    public static class AssertionHelpers
+    {
+        public static T WithRetries<T>(Func<T> query, int retries = 100)
+        {
+            while (true)
+            {
+                try
+                {
+                    return query();
+                }
+                catch (NoSuchElementException)
+                {
+                    if (retries > 0)
+                    {
+                        retries = SleepABit(retries);
+                    }
+                    else throw;
+                }
+            }
+        }
+
+        public static void WithRetries(Action action, int retries = 100)
+        {
+            while (true)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (AssertionException)
+                {
+                    if (retries > 0)
+                    {
+                        retries = SleepABit(retries);
+                    }
+                    else throw;
+                }
+                catch (NoSuchElementException)
+                {
+                    if (retries > 0)
+                    {
+                        retries = SleepABit(retries);
+                    }
+                    else throw;
+                }
+            }
+        }
+
+        static int SleepABit(int retries)
+        {
+            retries--;
+            Thread.Sleep(100);
+            return retries;
+        }
+    }
+}
