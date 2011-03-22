968f27bc23e8f103490a307218384c72809277e2
Petar Shomov <pshomov@gmail.com>
2011-03-22 21:25:39 +0000

    forgot to add these a few commits ago

---
+++ src/app/Frog.UI.Web/HttpHelpers/MonoBugs.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Web.Script.Serialization;
+using System.Web.Mvc;
+using System.Text;
+namespace Frog.UI.Web
+{
+	public class MonoBugs
+	{
+        public static ContentResult Json(object o)
+		{
+			var ser = new JavaScriptSerializer();
+        	return new ContentResult(){Content = ser.Serialize(o), ContentType = "application/json", ContentEncoding = Encoding.UTF8};
+		}
+	}
+}
+
