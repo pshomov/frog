a3e038c78cc318d984c334f38f758630d191cc71
Petar Shomov <petar@spretturmarimo.is>
2011-04-15 23:12:14 +0000

    testing handling of exceptions in the queue handlers, had to upgrade the AMQP protocol requirements to achieve poison queue effect

--- src/app/Frog.Domain.Integration/RabbitMQBus.cs
+++ src/app/Frog.Domain.Integration/RabbitMQBus.cs
@@ -1,4 +1,5 @@
 using System;
+using RabbitMQ.Client.Events;
 using SimpleCQRS;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Exceptions;
@@ -14,7 +15,7 @@ namespace Frog.Domain.Integration
 
 		public RabbitMQBus(string server)
 		{
-			var connectionFactory = new ConnectionFactory {HostName = server, UserName = "guest", Password = "guest",Protocol = Protocols.AMQP_0_8};
+			var connectionFactory = new ConnectionFactory {HostName = server, UserName = "guest", Password = "guest",Protocol = Protocols.AMQP_0_9_1};
 		    connection = connectionFactory.CreateConnection();
 		}
 
@@ -30,23 +31,32 @@ namespace Frog.Domain.Integration
 			var consumer = new QueueingBasicConsumer (channel);
 			var job = new Thread (() => {
 					var consumerTag = channel.BasicConsume(queueName, false, consumer);
-			        var jsonSerializer = new JavaScriptSerializer ();
+			        var jsonSerializer = new JavaScriptSerializer();
 			        while (true)
-					{
-					    try {
-							var e = (RabbitMQ.Client.Events.BasicDeliverEventArgs)consumer.Queue.Dequeue();
+			        {
+			            var e = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+			            try {
 					        var deserialized = jsonSerializer.Deserialize<T>(Encoding.UTF8.GetString(e.Body));
 							handler(deserialized);
 							channel.BasicAck(e.DeliveryTag, false);
 						} catch (OperationInterruptedException) {
 							break;
-						}
-					}
+						} catch(Exception ex)
+						{
+						   HandleBadMessage(e, ex);
+                           channel.BasicNack(e.DeliveryTag, false, true);
+                        }
+			        }
 			});
 			job.Start();
 		}
-		
-		void SendMessage<T>(T @event)
+
+	    private void HandleBadMessage(BasicDeliverEventArgs basicDeliverEventArgs, Exception exception)
+	    {    
+	        Console.WriteLine(string.Format(">>> Exception handling message {0}. Exception is {1}", basicDeliverEventArgs, exception));
+	    }
+
+	    void SendMessage<T>(T @event)
 		{
 				var channel = connection.CreateModel();
 				var topicName = typeof(T).Name;
--- src/tests/Frog.Domain.IntegrationTests/RabbitMQTests.cs
+++ src/tests/Frog.Domain.IntegrationTests/RabbitMQTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Frog.Specs.Support;
 using NUnit.Framework;
 using Frog.Domain.Integration;
@@ -33,6 +34,28 @@ namespace Frog.Domain.IntegrationTests
 		    AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("ff")));
 		    AssertionHelpers.WithRetries(() => Assert.That(msg2, Is.EqualTo("ff")));
 		}
+
+        [Test]
+		public void should_send_mesage_and_only_one_subscriber_of_the_queue_will_receive_it(){
+			var msg1 = "";
+			var msg2 = "";
+			bus.RegisterHandler<MyEvent>(ev =>  msg1 = ev.Data, "unit_test3");
+			bus.RegisterHandler<MyEvent>(ev =>  msg2 = ev.Data, "unit_test3");
+			bus.Publish(new MyEvent{Data = "ff"});
+            Thread.Sleep(3000);
+		    AssertionHelpers.WithRetries(() => Assert.That(msg1+msg2, Is.EqualTo("ff")));
+		}
+
+        [Test]
+		public void should_continue_handling_messages_after_particular_message_causes_exception(){
+			var msg1 = "";
+            bus.RegisterHandler<MyEvent>(ev => { if (ev.Data == "f1") throw new Exception("ouch"); else msg1 = ev.Data; }, "unit_test4");
+            bus.Publish(new MyEvent{Data = "f1"});
+            bus.Publish(new MyEvent{Data = "fd"});
+            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("fd")));
+            bus.RegisterHandler<MyEvent>(ev => msg1 = ev.Data, "unit_test4");
+            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("f1")));
+        }
 	}
 }
 
