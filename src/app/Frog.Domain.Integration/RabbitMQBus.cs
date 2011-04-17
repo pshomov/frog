3116430b25734b3adb71928298ba9404296731c0
Petar Shomov <petar@spretturmarimo.is>
2011-04-17 22:16:53 +0000

    need more work on this

--- src/app/Frog.Domain.Integration/RabbitMQBus.cs
+++ src/app/Frog.Domain.Integration/RabbitMQBus.cs
@@ -1,82 +1,106 @@
 using System;
-using RabbitMQ.Client.Events;
-using SimpleCQRS;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Exceptions;
+using System.IO;
+using System.Text;
 using System.Threading;
 using System.Web.Script.Serialization;
-using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using SimpleCQRS;
 
 namespace Frog.Domain.Integration
 {
-	public class RabbitMQBus : IBus, IBusDebug
-	{
-		IConnection connection;
+    public class RabbitMQBus : IBus, IBusDebug, IDisposable
+    {
+        private readonly IConnection connection;
 
-		public RabbitMQBus(string server)
-		{
-			var connectionFactory = new ConnectionFactory {HostName = server, UserName = "guest", Password = "guest",Protocol = Protocols.AMQP_0_9_1};
-		    connection = connectionFactory.CreateConnection();
-		}
+        public RabbitMQBus(string server)
+        {
+            var connectionFactory = new ConnectionFactory
+                                        {
+                                            HostName = server,
+                                            UserName = "guest",
+                                            Password = "guest",
+                                            Protocol = Protocols.AMQP_0_9_1
+                                        };
+            connection = connectionFactory.CreateConnection();
+        }
 
-		public void RegisterHandler<T>(Action<T> handler, string handlerId) where T : Message
-		{
-			var channel = connection.CreateModel();
-		    var topicName = typeof(T).Name;
-		    var queueName = string.Format("{0}_{1}", topicName, handlerId);
-		    channel.ExchangeDeclare(topicName, ExchangeType.Fanout, true);
-			channel.QueueDeclare(queueName, false, false, false, null);
-			channel.QueueBind(queueName, topicName, "", null);
+        public void RegisterHandler<T>(Action<T> handler, string handlerId) where T : Message
+        {
+            IModel channel = connection.CreateModel();
+            string topicName = typeof (T).Name;
+            string queueName = handlerId;
+            channel.ExchangeDeclare(topicName, ExchangeType.Fanout, true);
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, topicName, "", null);
 
-			var consumer = new QueueingBasicConsumer (channel);
-			var job = new Thread (() => {
-					var consumerTag = channel.BasicConsume(queueName, false, consumer);
-			        var jsonSerializer = new JavaScriptSerializer();
-			        while (true)
-			        {
-			            var e = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
-			            try {
-					        var deserialized = jsonSerializer.Deserialize<T>(Encoding.UTF8.GetString(e.Body));
-							handler(deserialized);
-							channel.BasicAck(e.DeliveryTag, false);
-						} catch (OperationInterruptedException) {
-							break;
-						} catch(Exception ex)
-						{
-						   HandleBadMessage(e, ex);
-                           channel.BasicNack(e.DeliveryTag, false, true);
-                        }
-			        }
-			});
-			job.Start();
-		}
+            var consumer = new QueueingBasicConsumer(channel);
+            var job = new Thread(() =>
+                                     {
+                                         string consumerTag = channel.BasicConsume(queueName, false, consumer);
+                                         var jsonSerializer = new JavaScriptSerializer();
+                                         while (true)
+                                         {
+                                             BasicDeliverEventArgs e = null;
+                                             try
+                                             {
+                                                 e = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                                                 var deserialized =
+                                                     jsonSerializer.Deserialize<T>(Encoding.UTF8.GetString(e.Body));
+                                                 handler(deserialized);
+                                                 channel.BasicAck(e.DeliveryTag, false);
+                                             }
+                                             catch(EndOfStreamException)
+                                             {
+                                                 break;
+                                             }
+                                             catch (OperationInterruptedException)
+                                             {
+                                                 break;
+                                             }
+                                             catch (Exception ex)
+                                             {
+                                                 HandleBadMessage(e, ex);
+                                                 channel.BasicReject(e.DeliveryTag, true);
+                                             }
+                                         }
+                                     });
+            job.Start();
+        }
 
-	    private void HandleBadMessage(BasicDeliverEventArgs basicDeliverEventArgs, Exception exception)
-	    {    
-	        Console.WriteLine(string.Format(">>> Exception handling message {0}. Exception is {1}", basicDeliverEventArgs, exception));
-	    }
+        public void Publish<T>(T @event) where T : Event
+        {
+            SendMessage(@event);
+        }
 
-	    void SendMessage<T>(T @event)
-		{
-				var channel = connection.CreateModel();
-				var topicName = typeof(T).Name;
-				var ser = new JavaScriptSerializer ();
-				var serializedForm = ser.Serialize(@event);
-				var basicProperties = channel.CreateBasicProperties();
-				channel.BasicPublish(topicName, "", false, false, basicProperties, Encoding.UTF8.GetBytes(serializedForm));
-		}
+        public void Send<T>(T command) where T : Command
+        {
+            SendMessage(command);
+        }
 
-		public void Publish<T>(T @event) where T : Event
-		{
-			SendMessage(@event);		
-		}
+        public event Action<Message> OnMessage;
 
-		public void Send<T>(T command) where T : Command
-		{
-			SendMessage(command);
-		}
+        private void HandleBadMessage(BasicDeliverEventArgs basicDeliverEventArgs, Exception exception)
+        {
+            Console.WriteLine(string.Format(">>> Exception handling message {0}. Exception is {1}",
+                                            basicDeliverEventArgs, exception));
+        }
 
-	    public event Action<Message> OnMessage;
-	}
+        private void SendMessage<T>(T @event)
+        {
+            IModel channel = connection.CreateModel();
+            string topicName = typeof (T).Name;
+            var ser = new JavaScriptSerializer();
+            string serializedForm = ser.Serialize(@event);
+            IBasicProperties basicProperties = channel.CreateBasicProperties();
+            channel.BasicPublish(topicName, "", false, false, basicProperties, Encoding.UTF8.GetBytes(serializedForm));
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
 }
-
--- src/tests/Frog.Domain.IntegrationTests/RabbitMQTests.cs
+++ src/tests/Frog.Domain.IntegrationTests/RabbitMQTests.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using Frog.Specs.Support;
 using NUnit.Framework;
@@ -10,18 +13,33 @@ namespace Frog.Domain.IntegrationTests
 	class MyEvent : Event{
 		public string Data;
 	}
+	class YourEvent : Event{
+		public string Data;
+	}
 
 	[TestFixture]
 	public class RabbitMQTests
 	{
-	    readonly IBus bus = new RabbitMQBus(Environment.GetEnvironmentVariable("RUNZ_TEST_RABBITMQ_SERVER") ?? "localhost");
+	    private RabbitMQBus bus;
+
+        [SetUp]
+        public void Setup()
+        {
+            bus = new RabbitMQBus(Environment.GetEnvironmentVariable("RUNZ_TEST_RABBITMQ_SERVER") ?? "localhost");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            bus.Dispose();
+        }
 
         [Test]
 		public void should_send_mesage_and_receive_it(){
 			var msg = "";
 			bus.RegisterHandler<MyEvent>(ev =>  msg = ev.Data, "unit_test0");
-			bus.Publish(new MyEvent{Data = "ff"});
-		    AssertionHelpers.WithRetries(() => Assert.That(msg, Is.EqualTo("ff")));
+			bus.Publish(new MyEvent{Data = "unit_test1"});
+            AssertionHelpers.WithRetries(() => Assert.That(msg, Is.EqualTo("unit_test1")));
 		}
 
         [Test]
@@ -30,9 +48,9 @@ namespace Frog.Domain.IntegrationTests
 			var msg2 = "";
 			bus.RegisterHandler<MyEvent>(ev =>  msg1 = ev.Data, "unit_test1");
 			bus.RegisterHandler<MyEvent>(ev =>  msg2 = ev.Data, "unit_test2");
-			bus.Publish(new MyEvent{Data = "ff"});
-		    AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("ff")));
-		    AssertionHelpers.WithRetries(() => Assert.That(msg2, Is.EqualTo("ff")));
+			bus.Publish(new MyEvent{Data = "unit_test2"});
+            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("unit_test2")));
+            AssertionHelpers.WithRetries(() => Assert.That(msg2, Is.EqualTo("unit_test2")));
 		}
 
         [Test]
@@ -41,20 +59,45 @@ namespace Frog.Domain.IntegrationTests
 			var msg2 = "";
 			bus.RegisterHandler<MyEvent>(ev =>  msg1 = ev.Data, "unit_test3");
 			bus.RegisterHandler<MyEvent>(ev =>  msg2 = ev.Data, "unit_test3");
-			bus.Publish(new MyEvent{Data = "ff"});
+            bus.Publish(new MyEvent { Data = "unit_test3" });
             Thread.Sleep(3000);
-		    AssertionHelpers.WithRetries(() => Assert.That(msg1+msg2, Is.EqualTo("ff")));
+            AssertionHelpers.WithRetries(() => Assert.That(msg1 + msg2, Is.EqualTo("unit_test3")));
 		}
 
         [Test]
 		public void should_continue_handling_messages_after_particular_message_causes_exception(){
 			var msg1 = "";
-            bus.RegisterHandler<MyEvent>(ev => { if (ev.Data == "f1") throw new Exception("ouch"); else msg1 = ev.Data; }, "unit_test4");
-            bus.Publish(new MyEvent{Data = "f1"});
-            bus.Publish(new MyEvent{Data = "fd"});
-            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("fd")));
+            bus.RegisterHandler<MyEvent>(ev => { if (ev.Data == "unit_test4_1") throw new Exception("ouch"); else msg1 = ev.Data; }, "unit_test4");
+            bus.Publish(new MyEvent { Data = "unit_test4_1" });
+            bus.Publish(new MyEvent { Data = "unit_test4_2" });
+            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("unit_test4_2")));
             bus.RegisterHandler<MyEvent>(ev => msg1 = ev.Data, "unit_test4");
-            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("f1")));
+            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("unit_test4_1")));
+        }
+
+        [Test]
+        public void should_handle_messages_sequentially_when_they_are_in_the_same_queue()
+        {
+            int messageCount = 0;
+            var messageAlreadyHandled = 0;
+            Action assertOtherMessageHasNotBeenHandledAlready = () => Assert.Fail("what the ..."); // weirdness so that we do not make assertions on a different thread
+            Action handler = () =>
+                                 {
+                                     if (messageCount++ == 0)
+                                     {
+                                         Thread.Sleep(1000);
+                                         var msg = messageAlreadyHandled;
+                                         assertOtherMessageHasNotBeenHandledAlready = () => Assert.That(msg, Is.EqualTo(0));
+                                     }
+                                     Interlocked.Increment(ref messageAlreadyHandled);
+                                 };
+            bus.RegisterHandler<MyEvent>(@event => handler(), "unit_test5");
+            bus.RegisterHandler<YourEvent>(@event => handler(), "unit_test5");
+            bus.Publish(new MyEvent { Data = "unit_test5_1" });
+            Thread.Sleep(50);
+            bus.Publish(new YourEvent { Data = "unit_test5_2" });
+            AssertionHelpers.WithRetries(() => Assert.That(messageAlreadyHandled, Is.EqualTo(2)));
+            assertOtherMessageHasNotBeenHandledAlready();
         }
 	}
 }
