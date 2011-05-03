18a5c210aaabcf3d6c7b036a64461583fad17596
Petar Shomov <petar@spretturmarimo.is>
2011-05-03 08:40:30 +0000

    putting the repository tracker in its own process/app

---
+++ src/app/Frog.RepositoryTracker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Frog.Domain.Integration;
+using SimpleCQRS;
+
+namespace Frog.RepositoryTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repoTracker = new Domain.RepositoryTracker(SetupBus());
+            repoTracker.StartListeningForBuildUpdates();
+            while(true)
+            {
+                repoTracker.CheckForUpdates();
+                Thread.Sleep(60*1000);
+            }
+        }
+        static IBus SetupBus()
+        {
+            return new RabbitMQBus(Environment.GetEnvironmentVariable("RUNZ_RABBITMQ_SERVER") ?? "localhost");
+        }
+    }
+}
