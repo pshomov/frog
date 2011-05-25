3a8797ca24fdbe69aa1371c980d7fdcada76fbec
Petar Shomov <petar@spretturmarimo.is>
2011-05-25 23:48:09 +0000

    small ref

--- src/app/Frog.Domain.Integration/RiakProjectRepository.cs
+++ src/app/Frog.Domain.Integration/RiakProjectRepository.cs
@@ -12,13 +12,12 @@ namespace Frog.Domain.Integration
 {
     public class RiakProjectRepository : IProjectsRepository
     {
-        private string BUCKET = "projects_test1";
-        JavaScriptSerializer jsonBridge = new JavaScriptSerializer();
+        private const string BUCKET = "projects_test1";
+        readonly JavaScriptSerializer jsonBridge = new JavaScriptSerializer();
 
         public void TrackRepository(string repoUrl)
         {
-            var connectionManager = RiakConnectionManager.FromConfiguration;
-            connectionManager.AddConnection("10.0.2.2", 8087);
+            var connectionManager = GetConnectionManager();
             var riakConnection = new RiakContentRepository(connectionManager);
             var response = riakConnection.Persist(new RiakPersistRequest
                                                       {
@@ -37,32 +36,36 @@ namespace Frog.Domain.Integration
         {
             get
             {
-                var connectionManager = RiakConnectionManager.FromConfiguration;
-                connectionManager.AddConnection("10.0.2.2", 8087); // server address and protocol buffer port
+                var connectionManager = GetConnectionManager();
                 var riakConnection = new RiakBucketRepository(connectionManager);
-                var keysFor = riakConnection.ListKeysFor(new ListKeysRequest() {Bucket = BUCKET.GetBytes()});
+                var keysFor = riakConnection.ListKeysFor(new ListKeysRequest {Bucket = BUCKET.GetBytes()});
                 if (keysFor.ResponseCode != RiakResponseCode.Successful)
                     throw new Exception("ouch, where is my data?");
                 var riakContentRepository = new RiakContentRepository(connectionManager);
                 var riakResponse =
-                    riakContentRepository.Find(new RiakFindRequest()
-                                                   {Bucket = BUCKET, Keys = keysFor.Result, ReadValue = 1});
+                    riakContentRepository.Find(new RiakFindRequest {Bucket = BUCKET, Keys = keysFor.Result, ReadValue = 1});
                 return riakResponse.Result.Select(document => jsonBridge.Deserialize<RepositoryDocument>(document.Value));
             }
         }
 
         public void UpdateLastKnownRevision(string repoUrl, string revision)
         {
-            var connectionManager = RiakConnectionManager.FromConfiguration;
-            connectionManager.AddConnection("10.0.2.2", 8087);
+            RiakConnectionManager connectionManager = GetConnectionManager();
             var riakConnection = new RiakContentRepository(connectionManager);
-            var riakResponse = riakConnection.Find(new RiakFindRequest() { Bucket = BUCKET, Keys = new []{KeyGenerator(repoUrl)}, ReadValue = 1 });
+            var riakResponse = riakConnection.Find(new RiakFindRequest { Bucket = BUCKET, Keys = new []{KeyGenerator(repoUrl)}, ReadValue = 1 });
             var doc = jsonBridge.Deserialize<RepositoryDocument>(riakResponse.Result[0].Value);
             doc.LastBuiltRevision = revision;
-            riakConnection.Persist(new RiakPersistRequest(){Bucket = BUCKET, Key = KeyGenerator(repoUrl), Content = new RiakContent(){Value = jsonBridge.Serialize(doc).GetBytes()}});
+            riakConnection.Persist(new RiakPersistRequest {Bucket = BUCKET, Key = KeyGenerator(repoUrl), Content = new RiakContent {Value = jsonBridge.Serialize(doc).GetBytes()}});
         }
 
-        private string KeyGenerator(string repoUrl)
+        private static RiakConnectionManager GetConnectionManager()
+        {
+            var connectionManager = RiakConnectionManager.FromConfiguration;
+            connectionManager.AddConnection("10.0.2.2", 8087);
+            return connectionManager;
+        }
+
+        private static string KeyGenerator(string repoUrl)
         {
             return string.Concat(
                 new MD5CryptoServiceProvider().ComputeHash(
