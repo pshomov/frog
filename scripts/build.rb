fb406920bc17e2c9eb8d14149f448498407104a5
Petar Shomov <petar@sprettur.is>
2011-06-25 18:29:02 +0000

    adding the possibility to build in both Release and Debug mode

--- scripts/build.rb
+++ scripts/build.rb
@@ -10,8 +10,8 @@ def red(text); colorize(text, "31"); end
 def green(text); colorize(text, "32"); end
 
 remove_dir "../output", :force => true
-
-system "xbuild ../Frog.Net.sln /target:rebuild /property:Configuration=Release"
+build_mode="Debug"
+system "xbuild ../Frog.Net.sln /target:rebuild /property:Configuration=#{build_mode}"
 status = $?.exitstatus
 if status != 0
 	puts(red("No Runz!"))
@@ -21,8 +21,8 @@ end
 mkdir "../output"
 cp_r "../src/app/Frog.UI.Web", "../output/web"
 cp_r "../src/app/Web", "../output/webz"
-cp_r "../src/app/Frog.Agent/bin/Release", "../output/agent"
-cp_r "../src/app/Frog.RepositoryTracker/bin/Release", "../output/repotracker"
+cp_r "../src/app/Frog.Agent/bin/#{build_mode}", "../output/agent"
+cp_r "../src/app/Frog.RepositoryTracker/bin/#{build_mode}", "../output/repotracker"
 rm Dir.glob('../output/web/**/*.cs')
 rm Dir.glob('../output/web/**/*.csproj')
 rm Dir.glob('../output/web/**/*.pidb')
