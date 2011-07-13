1c356540f88e8b3ea71e844812d1ee2c8dce9147
Petar Shomov <petar@sprettur.is>
2011-07-13 17:23:35 +0000

    adding getting the cpu time for all children in the process tree. Works for Win, let's see it work in unix too Mental note: reformating goes into a separate commit. Usually is very invasive.

--- src/app/Frog.Support/ProcessWrapper.cs
+++ src/app/Frog.Support/ProcessWrapper.cs
@@ -2,16 +2,17 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Frog.Support
 {
     public interface IProcessWrapper
     {
-        event Action<string> OnErrorOutput;
-        event Action<string> OnStdOutput;
         TimeSpan TotalProcessorTime { get; }
         int ExitCode { get; }
         int Id { get; }
+        event Action<string> OnErrorOutput;
+        event Action<string> OnStdOutput;
         void Execute();
         bool WaitForProcess(int timeoutMilliseconds);
         void Kill();
@@ -20,10 +21,14 @@ namespace Frog.Support
 
     public class ProcessWrapper : IProcessWrapper
     {
-        public event Action<string> OnErrorOutput = s => Console.WriteLine(String.Format("E>{0}", s));
-        public event Action<string> OnStdOutput = s => Console.WriteLine(String.Format("S>{0}", s));
+        private readonly string arguments;
+        private readonly string cmdExe;
+        private readonly string workingDirectory;
 
-        public ProcessWrapper(string cmdExe, string arguments) : this(cmdExe, arguments, Directory.GetCurrentDirectory())
+        private Process process;
+
+        public ProcessWrapper(string cmdExe, string arguments)
+            : this(cmdExe, arguments, Directory.GetCurrentDirectory())
         {
         }
 
@@ -34,25 +39,31 @@ namespace Frog.Support
             this.workingDirectory = workingDirectory;
         }
 
-        public Process ProcessInfo  
+        public Process ProcessInfo
         {
             get { return process; }
         }
 
+        public bool HasExited
+        {
+            get { return process.HasExited; }
+        }
+
+        #region IProcessWrapper Members
+
+        public event Action<string> OnErrorOutput = s => Console.WriteLine(String.Format("E>{0}", s));
+        public event Action<string> OnStdOutput = s => Console.WriteLine(String.Format("S>{0}", s));
+
         public TimeSpan TotalProcessorTime
         {
-            get {
-				if (Os.IsUnix){
-					var p = new ProcessWrapper("ps", String.Format("-p {0} -o time=", process.Id));
-					var time = "";
-					p.OnStdOutput += s => time = s ?? "";
-					p.Execute();
-					p.WaitForProcess();
-					if (time.Length > 0){
-						return TimeSpan.Parse(time);
-					} else return new TimeSpan(0);
-				} else
-                    return process.TotalProcessorTime;
+            get
+            {
+                if (Os.IsUnix)
+                {
+                    return UnixSpecific.UnixTotalProcessorTime(process.Handle.ToInt32());
+                }
+                else
+                    return WindowsTotalProcessorTime();
             }
         }
 
@@ -64,12 +75,12 @@ namespace Frog.Support
         public void Execute()
         {
             var psi = new ProcessStartInfo(cmdExe, arguments)
-            {
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                WorkingDirectory = workingDirectory
-            };
+                          {
+                              RedirectStandardOutput = true,
+                              RedirectStandardError = true,
+                              UseShellExecute = false,
+                              WorkingDirectory = workingDirectory
+                          };
 
             try
             {
@@ -86,29 +97,9 @@ namespace Frog.Support
             process.BeginOutputReadLine();
         }
 
-        public int WaitForProcess()
-        {
-            process.WaitForExit();
-            MonoBugFix(process.ExitCode);
-            return process.ExitCode;
-        }
-
-        readonly string cmdExe;
-
-        readonly string arguments;
-
-        readonly string workingDirectory;
-
-        Process process;
-
         public bool WaitForProcess(int timeoutMilliseconds)
         {
             return process.WaitForExit(timeoutMilliseconds);
-        }
-
-        private void MonoBugFix(int exitcode)
-        {
-            if (exitcode == 1) throw new ApplicationNotFoundException(cmdExe,null);
         }
 
         public void Kill()
@@ -123,18 +114,75 @@ namespace Frog.Support
             }
         }
 
-        public int Id { get { return process.Id; } }
-        public bool HasExited { get { return process.HasExited; } }
+        public int Id
+        {
+            get { return process.Id; }
+        }
 
         public void MakeSureTerminalOutputIsFlushed()
         {
             WaitForProcess();
         }
+
+        #endregion
+
+        private TimeSpan WindowsTotalProcessorTime()
+        {
+            Process[] processes = Process.GetProcesses();
+            TimeSpan cputime = GetProcessCpuTime(processes, process);
+            return cputime;
+        }
+
+        private TimeSpan GetProcessCpuTime(Process[] processes, Process process)
+        {
+            TimeSpan cputime = WinProcessProcessorTime(process);
+            cputime += WinChildProcessesProcessorTime(processes, process);
+            return cputime;
+        }
+
+        private TimeSpan WinChildProcessesProcessorTime(Process[] processes, Process process)
+        {
+            return
+                TimeSpan.FromTicks(
+                    processes.Where(p =>
+                                        {
+                                            try
+                                            {
+                                                Process parentProcess = ParentProcessUtilities.GetParentProcess(p.Handle);
+                                                return (parentProcess != null) &&
+                                                       (parentProcess.Handle == process.Handle);
+                                            }
+                                            catch (Win32Exception e)
+                                            {
+                                                if (e.NativeErrorCode == 5) return false;
+                                                throw;
+                                            }
+                                        }).
+                        Sum(childProcess => GetProcessCpuTime(processes, childProcess).Ticks));
+        }
+
+        private TimeSpan WinProcessProcessorTime(Process process)
+        {
+            return process.TotalProcessorTime;
+        }
+
+        public int WaitForProcess()
+        {
+            process.WaitForExit();
+            MonoBugFix(process.ExitCode);
+            return process.ExitCode;
+        }
+
+        private void MonoBugFix(int exitcode)
+        {
+            if (exitcode == 1) throw new ApplicationNotFoundException(cmdExe, null);
+        }
     }
 
     public class ApplicationNotFoundException : Exception
     {
-        public ApplicationNotFoundException(string application, Exception inner) : base(string.Format("Error launching application {0}", application), inner)
+        public ApplicationNotFoundException(string application, Exception inner)
+            : base(string.Format("Error launching application {0}", application), inner)
         {
         }
     }
---
+++ src/app/Frog.Support/UnixSpecific.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Frog.Support
+{
+    class UnixSpecific
+    {
+        public static TimeSpan UnixTotalProcessorTime(int processId)
+        {
+            var p = new ProcessWrapper("ps", "-o pid=,ppid=,time=");
+            var processStrings = new List<String>();
+            p.OnStdOutput += s => { if (!s.IsNullOrEmpty()) processStrings.Add(s); };
+            p.Execute();
+            p.WaitForProcess();
+            var processes = ParsePSInfo(processStrings);
+            var cputime = GetTotalCpuTime(processes, processId);
+            return cputime;
+        }
+
+        private static TimeSpan GetTotalCpuTime(List<long[]> processes, int pid)
+        {
+            var cputime = GetCpuTimeFor(processes, pid);
+            cputime += GetChildrenCpuTimeFor(processes, pid);
+            return cputime;
+        }
+
+        internal static TimeSpan GetChildrenCpuTimeFor(List<long[]> processes, int pid)
+        {
+            return TimeSpan.FromTicks(processes.Where(longs => longs[1] == pid).Sum(longs => GetTotalCpuTime(processes, (int) longs[0]).Ticks));
+        }
+
+        internal static TimeSpan GetCpuTimeFor(IEnumerable<long[]> processes, int pid)
+        {
+            return TimeSpan.FromTicks(processes.Single(ints => ints[0] == pid)[2]);
+        }
+
+        internal static List<long[]> ParsePSInfo(List<string> processStrings)
+        {
+            var processes = new List<long[]>();
+            processStrings.ForEach(s =>
+                                       {
+                                           var strings = s.Split(' ');
+                                           var pid = Int32.Parse(strings[0]);
+                                           var ppid = Int32.Parse(strings[1]);
+                                           var cpu = TimeSpan.Parse(strings[2]).Ticks;
+                                           processes.Add(new[]{pid, ppid, cpu});
+                                       });
+            return processes;
+        }
+    }
+}
