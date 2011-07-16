f3120e7cec6d9505908bcb2a3e005737aa1f0862
Petar Shomov <petar@sprettur.is>
2011-07-16 21:48:51 +0000

    supporting the time format of both OSX and Linux "ps" command

--- src/app/Frog.Support/UnixSpecific.cs
+++ src/app/Frog.Support/UnixSpecific.cs
@@ -10,7 +10,7 @@ namespace Frog.Support
     {
         public static TimeSpan UnixTotalProcessorTime(int processId)
         {
-            var p = new ProcessWrapper("ps", "-ax -o pid=,ppid=,time=");
+            var p = new ProcessWrapper("ps", "ax -o pid=,ppid=,time=");
             var processStrings = new List<String>();
             p.OnStdOutput += s => { if (!s.IsNullOrEmpty()) processStrings.Add(s); };
             p.Execute();
@@ -45,8 +45,14 @@ namespace Frog.Support
                                            var strings = s.Trim().Replace("    ", " ").Replace("   ", " ").Replace("  ", " " ).Split(' ');
                                            var pid = Int32.Parse(strings[0]);
                                            var ppid = Int32.Parse(strings[1]);
-										   var p = Regex.Match(strings[2], @"(\d+):(\d+)\.(\d+)");
-                                           var cpu = TimeSpan.FromMinutes(Int32.Parse(p.Groups[1].Value)).Add(TimeSpan.FromSeconds(Int32.Parse(p.Groups[2].Value))).Add(TimeSpan.FromMilliseconds(Int32.Parse(p.Groups[3].Value))).Ticks;
+										   long cpu;	
+										   if (!strings[2].Contains(".")) {
+										     var p = Regex.Match(strings[2], @"(\d+):(\d+)\:(\d+)");
+                                             cpu = TimeSpan.FromHours(Int32.Parse(p.Groups[1].Value)).Add(TimeSpan.FromMinutes(Int32.Parse(p.Groups[2].Value))).Add(TimeSpan.FromSeconds(Int32.Parse(p.Groups[3].Value))).Ticks;
+				                           } else {
+										     var p = Regex.Match(strings[2], @"(\d+):(\d+)\.(\d+)");
+                                             cpu = TimeSpan.FromMinutes(Int32.Parse(p.Groups[1].Value)).Add(TimeSpan.FromSeconds(Int32.Parse(p.Groups[2].Value))).Add(TimeSpan.FromMilliseconds(Int32.Parse(p.Groups[3].Value))).Ticks;
+											}	
                                            processes.Add(new[]{pid, ppid, cpu});
                                        });
             return processes;
