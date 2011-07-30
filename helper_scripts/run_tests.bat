d6ef35e1488a219c9efa951bf7a6309a34d143ec
Petar Shomov <petar@sprettur.is>
2011-07-30 07:53:19 +0000

    updating the unit and system tests runner script to skip running mspec tests

--- helper_scripts/run_tests.bat
+++ helper_scripts/run_tests.bat
@@ -1,7 +1,5 @@
 libs\NUnit\nunit-console.exe src\tests\Frog.Domain.Specs\bin\Debug\Frog.Domain.Test.dll
 @if %errorlevel% neq 0 exit /b %errorlevel%
-libs\mspec\mspec.exe src\tests\Frog.Domain.Specs\bin\Debug\Frog.Domain.Test.dll
-@if %errorlevel% neq 0 exit /b %errorlevel%
 libs\NUnit\nunit-console.exe src\tests\Frog.System.Specs\bin\Debug\Frog.System.Test.dll
 echo "Error level is " %errorlevel%
 @if %errorlevel% neq 0 exit /b %errorlevel%
