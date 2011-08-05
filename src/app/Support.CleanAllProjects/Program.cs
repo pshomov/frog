e06f1d109aaa00c2e0f8f64a48833c41704cca7c
Petar Shomov <petar@sprettur.is>
2011-08-05 15:42:16 +0000

    adding a helper program to remove all the registered projects from the Data store to help with the acceptance testing

---
+++ src/app/Support.CleanAllProjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Frog.Domain.Integration;
+using Frog.Support;
+
+namespace Support.CleanAllProjects
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var riak = new RiakProjectRepository(
+                OSHelpers.RiakHost(),
+                OSHelpers.RiakPort(),
+                "projects");
+            riak.AllProjects.ToList().ForEach(document => riak.RemoveProject(document.projecturl));
+        }
+    }
+}
