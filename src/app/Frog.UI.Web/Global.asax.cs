f5273a539fa2a890b40f067df6b77c1650eb7d99
Petar Shomov <petar@sprettur.is>
2011-08-05 15:40:41 +0000

    removing unused MVC routing

--- src/app/Frog.UI.Web/Global.asax.cs
+++ src/app/Frog.UI.Web/Global.asax.cs
@@ -24,18 +24,6 @@ namespace Frog.UI.Web
             filters.Add(new HandleErrorAttribute());
         }
 
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-        }
-
         protected void Application_Start()
         {
             serviceArea = Server.MapPath("~/App_Data");
