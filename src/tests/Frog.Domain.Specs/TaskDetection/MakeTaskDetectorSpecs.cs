c38364b6973eef4e0db3ff21b46c4acf63c5eacf
Petar Shomov <petar@sprettur.is>
2011-09-12 09:23:53 +0000

    and here comes the task detector for the make-based projects

---
+++ src/tests/Frog.Domain.Specs/TaskDetection/MakeTaskDetectorSpecs.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Frog.Domain.BuildSystems.Make;
+using Frog.Domain.CustomTasks;
+using Frog.Specs.Support;
+using Frog.Support;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Frog.Domain.Specs.TaskDetection
+{
+    [TestFixture]
+    public class MakeTaskDetectorSpecs : BDD
+    {
+        private TaskFileFinder projectTaskFileRepo;
+        private MakeTaskDetector makeTaskDetector;
+        private IList<ITask> items;
+
+        protected override void Given()
+        {
+            projectTaskFileRepo = Substitute.For<TaskFileFinder>();
+            makeTaskDetector = new MakeTaskDetector(projectTaskFileRepo);
+            projectTaskFileRepo.FindFiles("basefolder").Returns(As.List(Os.DirChars("makefile")));
+        }
+
+        protected override void When()
+        {
+            items = makeTaskDetector.Detect("basefolder");
+        }
+
+        [Test]
+        public void should_have_only_one_task()
+        {
+            Assert.That(items.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void should_be_a_make_task()
+        {
+            Assert.That((items[0] as MakeTask), Is.Not.Null);
+        }
+    }
+}
