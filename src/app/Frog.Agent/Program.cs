3e6207c691ce2117022fd7f357b09ac3c33e394d
Petar Shomov <petar@sprettur.is>
2011-10-11 08:46:39 +0000

    adding an event archiver since viewing the events from the RabbitMQ UI requeues them and we loose the original order

--- src/app/Frog.Agent/Program.cs
+++ src/app/Frog.Agent/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using Frog.Domain;
 using Frog.Domain.BuildSystems.FrogSystemTest;
 using Frog.Domain.BuildSystems.Make;
 using Frog.Domain.BuildSystems.Rake;
 using Frog.Domain.BuildSystems.Solution;
+using Frog.Domain.EventsArchiver;
 using Frog.Domain.Integration;
 using Frog.Domain.RevisionChecker;
 using Frog.Domain.TaskSources;
@@ -15,7 +13,7 @@ using SimpleCQRS;
 
 namespace Frog.Agent
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
@@ -24,6 +22,8 @@ namespace Frog.Agent
             SourceRepoDriverFactory sourceRepoDriverFactory = url => new GitDriver(url);
             var agent = new Domain.Agent(bus, worker, sourceRepoDriverFactory);
             var revisionChecker = new RevisionChecker(bus, sourceRepoDriverFactory);
+
+            new EventsArchiver(bus).JoinTheParty();
             agent.JoinTheParty();
             revisionChecker.JoinTheParty();
             Console.ReadLine();
---
+++ src/app/Frog.Domain/EventsArchiver/EventsArchiver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web.Script.Serialization;
+using SimpleCQRS;
+
+namespace Frog.Domain.EventsArchiver
+{
+    public class EventsArchiver : Handles<Message>
+    {
+        private readonly IBus bus;
+        readonly JavaScriptSerializer jsonBridge = new JavaScriptSerializer();
+
+        public EventsArchiver(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public void Handle(Message message)
+        {
+            File.AppendAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "runz_events.log"), jsonBridge.Serialize(message));
+        }
+
+        public void JoinTheParty()
+        {
+            bus.RegisterHandler<Message>(this.Handle, "all_messages");
+        }
+    }
+}
