d6661f6d3a5fafc9ba90c16696ed2d0a613db81c
Petar Shomov <petar@sprettur.is>
2011-12-21 09:27:18 +0000

    * removing some unused reference * removing duplication

--- src/app/Frog.Domain.Integration/RiakProjectRepository.cs
+++ src/app/Frog.Domain.Integration/RiakProjectRepository.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using CorrugatedIron;
 using Frog.Domain.RepositoryTracker;
 
 namespace Frog.Domain.Integration
@@ -10,6 +10,7 @@ namespace Frog.Domain.Integration
         private readonly string host;
         private readonly int port;
         private readonly string bucket;
+        private IRiakClient iRiakClient;
 
         public RiakProjectRepository(string host, int port, string bucket)
         {
@@ -20,7 +21,7 @@ namespace Frog.Domain.Integration
 
         public void TrackRepository(string repoUrl)
         {
-            var client = Riak.GetConnectionManager(host, port).CreateClient();
+            var client = CreateClient();
             client.Put(bucket, Riak.KeyGenerator(repoUrl),
                        new RepositoryDocument {revision = "", projecturl = repoUrl});
         }
@@ -29,7 +30,7 @@ namespace Frog.Domain.Integration
         {
             get
             {
-                var client = Riak.GetConnectionManager(host, port).CreateClient();
+                var client = CreateClient();
                 var keys = client.ListKeyz(bucket);
                 return keys.Select(s => client.Get<RepositoryDocument>(bucket, s));
             }
@@ -37,7 +38,7 @@ namespace Frog.Domain.Integration
 
         public void UpdateLastKnownRevision(string repoUrl, string revision)
         {
-            var connectionManager = Riak.GetConnectionManager(host, port).CreateClient();
+            var connectionManager = CreateClient();
             var doc = connectionManager.Get<RepositoryDocument>(bucket, Riak.KeyGenerator(repoUrl));
             doc.revision = revision;
             doc.CheckForUpdateRequested = false;
@@ -46,13 +47,13 @@ namespace Frog.Domain.Integration
 
         public void RemoveProject(string repoUrl)
         {
-            var connectionManager = Riak.GetConnectionManager(host, port).CreateClient();
+            var connectionManager = CreateClient();
             connectionManager.Delete(bucket, Riak.KeyGenerator(repoUrl));
         }
 
         public void ProjectCheckInProgress(string repoUrl)
         {
-            var client = Riak.GetConnectionManager(host, port).CreateClient();
+            var client = CreateClient();
             var doc = client.Get<RepositoryDocument>(bucket, Riak.KeyGenerator(repoUrl));
             doc.CheckForUpdateRequested = true;
             client.Put(bucket, Riak.KeyGenerator(repoUrl), doc);
@@ -60,7 +61,7 @@ namespace Frog.Domain.Integration
 
         public void ProjectCheckComplete(string repoUrl)
         {
-            var client = Riak.GetConnectionManager(host, port).CreateClient();
+            var client = CreateClient();
             var doc = client.Get<RepositoryDocument>(bucket, Riak.KeyGenerator(repoUrl));
             doc.CheckForUpdateRequested = false;
             client.Put(bucket, Riak.KeyGenerator(repoUrl), doc);
@@ -68,8 +69,15 @@ namespace Frog.Domain.Integration
 
         public void WipeBucket()
         {
-            var client = Riak.GetConnectionManager(host, port).CreateClient();
+            var client = CreateClient();
             client.DeleteBucket(bucket);
+        }
+
+        private IRiakClient CreateClient()
+        {
+            return Riak.GetConnectionManager(host, port).CreateClient();
+            if (iRiakClient == null) iRiakClient = Riak.GetConnectionManager(host, port).CreateClient();
+            return iRiakClient;
         }
     }
 }
