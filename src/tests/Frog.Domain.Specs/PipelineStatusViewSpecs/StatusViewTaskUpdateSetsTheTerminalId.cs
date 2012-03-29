0f689ea8eaa36fe92fbe0a7556e47ec7c128693b
Petar Shomov <petar@sprettur.is>
2012-03-29 09:22:01 +0000

    ref

--- src/tests/Frog.Domain.Specs/PipelineStatusViewSpecs/StatusViewTaskUpdateSetsTheTerminalId.cs
+++ src/tests/Frog.Domain.Specs/PipelineStatusViewSpecs/StatusViewTaskUpdateSetsTheTerminalId.cs
@@ -5,29 +5,28 @@ namespace Frog.Domain.Specs.PipelineStatusViewSpecs
 {
     class StatusViewTaskUpdateSetsTheTerminalId : StatusViewCurrentBuildPublicRepoBase
     {
-        private Guid terminalId;
-        private Guid terminalId_task1;
-        private Guid terminalId_task2;
+        private Guid terminal_id_task1;
+        private Guid terminal_id_task2;
 
         protected override void Given()
         {
             base.Given();
             RepoUrl = "http://";
             HandleProjectCheckedOut("");
-            terminalId_task1 = Guid.NewGuid();
-            terminalId_task2 = Guid.NewGuid();
+            terminal_id_task1 = Guid.NewGuid();
+            terminal_id_task2 = Guid.NewGuid();
         }
 
         protected override void When()
         {
-            HandleBuildStarted(new TaskInfo("t1", terminalId_task1), new TaskInfo("t2", terminalId_task2));
+            HandleBuildStarted(new TaskInfo("t1", terminal_id_task1), new TaskInfo("t2", terminal_id_task2));
         }
 
         [Test]
         public void should_have_terminalId_as_specified_in_the_message()
         {
-            Assert.That(ProjectView.GetBuildStatus(BuildMessage.BuildId).Tasks[0].TerminalId, Is.EqualTo(terminalId_task1));
-            Assert.That(ProjectView.GetBuildStatus(BuildMessage.BuildId).Tasks[1].TerminalId, Is.EqualTo(terminalId_task2));
+            Assert.That(ProjectView.GetBuildStatus(BuildMessage.BuildId).Tasks[0].TerminalId, Is.EqualTo(terminal_id_task1));
+            Assert.That(ProjectView.GetBuildStatus(BuildMessage.BuildId).Tasks[1].TerminalId, Is.EqualTo(terminal_id_task2));
         }
     }
 }
