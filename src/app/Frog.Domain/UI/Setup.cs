40cf85df1526e070a1f827ecf2770c55237e7e56
Petar Shomov <petar@sprettur.is>
2012-04-03 22:01:44 +0000

    cleaming up

--- src/app/Frog.Domain/UI/Setup.cs
+++ src/app/Frog.Domain/UI/Setup.cs
@@ -3,7 +3,7 @@ using SimpleCQRS;
 
 namespace Frog.Domain.UI
 {
-    public class Setup
+    public static class Setup
     {
         public static void SetupView(IBus theBus, IStoreEvents eventStore)
         {
@@ -17,7 +17,6 @@ namespace Frog.Domain.UI
             theBus.RegisterHandler<BuildStarted>(statusView.Handle, "UI");
             theBus.RegisterHandler<BuildEnded>(statusView.Handle, "UI");
             theBus.RegisterHandler<BuildUpdated>(statusView.Handle, "UI");
-//            theBus.RegisterHandler<TerminalUpdate>(statusView.Handle, "UI");
             theBus.RegisterHandler<ProjectCheckedOut>(statusView.Handle, "UI");
         }
 
