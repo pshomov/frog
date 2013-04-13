4977ac27a0a5f8dd60d0c80aae51768fe80d0142
Petar Shomov <pshomov@gmail.com>
2013-04-13 16:04:08 +0000

    working on the configuration lib

---
+++ src/app/Frog.Support/Config.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Frog.Support
+{
+    public class Config : DynamicObject
+    {
+        private readonly string appHome;
+        private readonly JObject cfg;
+
+        public Config(string app_home)
+        {
+            appHome = app_home;
+            cfg = JObject.Parse(File.ReadAllText(Path.Combine(appHome, "config.json")));
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            JToken val;
+            var has_value = cfg.TryGetValue(binder.Name, out val);
+            if (has_value) 
+                result = val.Value<object>();
+            else 
+                throw new SettingNotDefined();
+            return has_value;
+        }
+    }
+
+    public class SettingNotDefined : Exception
+    {
+    }
+
+}
---
+++ src/tests/Frog.Domain.IntegrationTests/Configuration/ConfigReadsJson.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Frog.Specs.Support;
+using Frog.Support;
+using NUnit.Framework;
+
+namespace Frog.Domain.IntegrationTests.Configuration
+{
+    class ConfigReadsJson : BDD
+    {
+        private dynamic config;
+        private string directory;
+
+        protected override void Given()
+        {
+            directory = GetTemporaryDirectory();
+            var genesis = new FileGenesis(directory);
+            genesis.File("config.json", "{setting1 : 'v1'}");
+        }
+
+        protected override void When()
+        {
+            config = new Config(directory);
+        }
+
+        [Test]
+        public void should_read_the_value_from_the_config()
+        {
+            Assert.That(config.setting1, Is.EqualTo("v1"));
+        }
+
+        [Test]
+        public void should_throw_exception_when_setting_not_defined()
+        {
+            try
+            {
+                var a = config.setting2;
+            }
+            catch (SettingNotDefined)
+            {
+            }
+        }
+
+        string GetTemporaryDirectory()
+        {
+            string temp_directory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(temp_directory);
+            return temp_directory;
+        }
+    }
+
+}
