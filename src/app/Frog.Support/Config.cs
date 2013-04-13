aeab86a3d93dde9982694bf283eafa0755c75af2
Petar Shomov <pshomov@gmail.com>
2013-04-13 16:45:20 +0000

    adding hierarchy of json support to the configuration thing

--- src/app/Frog.Support/Config.cs
+++ src/app/Frog.Support/Config.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace Frog.Support
@@ -8,23 +10,35 @@ namespace Frog.Support
     public class Config : DynamicObject
     {
         private readonly string appHome;
-        private readonly JObject cfg;
+        private readonly List<JObject> cfg  = new List<JObject>();
 
         public Config(string app_home)
         {
             appHome = app_home;
-            cfg = JObject.Parse(File.ReadAllText(Path.Combine(appHome, "config.json")));
+            AddConfigIfPresent(appHome);
+            DirectoryInfo parent = new DirectoryInfo(app_home);
+            while((parent = Directory.GetParent(app_home)) != null)
+            {
+                app_home = parent.FullName;
+                AddConfigIfPresent(app_home);
+            }
+        }
+
+        private void AddConfigIfPresent(string directory)
+        {
+            var path = Path.Combine(directory, "config.json");
+            if (File.Exists(path))
+                cfg.Add(JObject.Parse(File.ReadAllText(path)));
         }
 
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             JToken val;
-            var has_value = cfg.TryGetValue(binder.Name, out val);
-            if (has_value) 
-                result = val.Value<object>();
-            else 
-                throw new SettingNotDefined();
-            return has_value;
+            var setting_values = cfg.Where(o => o.TryGetValue(binder.Name, out val));
+            if (setting_values.Count() == 0) throw new SettingNotDefined();
+            setting_values.First().TryGetValue(binder.Name, out val);
+            result = val.Value<object>();
+            return true;
         }
     }
 
---
+++ src/tests/Frog.Domain.IntegrationTests/Configuration/ConfigReadsHierarchyOfJson.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using Frog.Specs.Support;
+using Frog.Support;
+using NUnit.Framework;
+
+namespace Frog.Domain.IntegrationTests.Configuration
+{
+    class ConfigReadsHierarchyOfJson : BDD
+    {
+        private dynamic config;
+        private string directory;
+        private string sub_folder_name = "sub";
+
+        protected override void Given()
+        {
+            directory = GetTemporaryDirectory();
+            var genesis = new FileGenesis(directory);
+            genesis.File("config.json", "{setting1 : 'v1', setting2 : 'wow' }")
+                   .Folder(sub_folder_name)
+                   .File("config.json", "{setting1 : 'v2'}");
+        }
+
+        protected override void When()
+        {
+            config = new Config(Path.Combine(directory, sub_folder_name));
+        }
+
+        [Test]
+        public void should_use_the_setting_value_thats_closest_to_the_app()
+        {
+            Assert.That(config.setting1, Is.EqualTo("v2"));
+        }
+
+        [Test]
+        public void should_use_the_setting_value_found_in_parent_when_no_value_present_locally()
+        {
+            Assert.That(config.setting2, Is.EqualTo("wow"));
+        }
+
+        string GetTemporaryDirectory()
+        {
+            string temp_directory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(temp_directory);
+            return temp_directory;
+        }
+    }
+
+}
