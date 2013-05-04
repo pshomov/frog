dc254118456ecc31addf0aabddcbb92f24cb336a
Petar Shomov <pshomov@gmail.com>
2013-05-04 17:32:56 +0000

    refactoring the MQ tests a bit

--- src/tests/Frog.Domain.IntegrationTests/RabbitMQ/RabbitMQBasicMessagingTest.cs
+++ src/tests/Frog.Domain.IntegrationTests/RabbitMQ/RabbitMQBasicMessagingTest.cs
@@ -19,6 +19,13 @@ namespace Frog.Domain.IntegrationTests.RabbitMQ
 	[TestFixture]
 	public class RabbitMQBasicMessagingTest
 	{
+	    private const string unitTest7 = "unit_test7";
+	    private const string unitTest5 = "unit_test5";
+	    private const string unitTest4 = "unit_test4";
+	    private const string unitTest3 = "unit_test3";
+	    private const string unitTest2 = "unit_test2";
+	    private const string unitTest1 = "unit_test1";
+	    private const string unitTest0 = "unit_test0";
 	    private RabbitMQBus bus1;
 	    private RabbitMQBus bus2;
 
@@ -27,12 +34,13 @@ namespace Frog.Domain.IntegrationTests.RabbitMQ
         {
             bus1 = new RabbitMQBus(Environment.GetEnvironmentVariable("RUNZ_TEST_RABBITMQ_SERVER") ?? "localhost");
             bus2 = new RabbitMQBus(Environment.GetEnvironmentVariable("RUNZ_TEST_RABBITMQ_SERVER") ?? "localhost");
-            bus1.PurgeQueue("unit_test1");
-            bus1.PurgeQueue("unit_test2");
-            bus1.PurgeQueue("unit_test3");
-            bus1.PurgeQueue("unit_test4");
-            bus1.PurgeQueue("unit_test5");
-            bus1.PurgeQueue("unit_test7");
+            bus1.PurgeQueue(unitTest0);
+            bus1.PurgeQueue(unitTest1);
+            bus1.PurgeQueue(unitTest2);
+            bus1.PurgeQueue(unitTest3);
+            bus1.PurgeQueue(unitTest4);
+            bus1.PurgeQueue(unitTest5);
+            bus1.PurgeQueue(unitTest7);
         }
 
         [TearDown]
@@ -47,37 +55,38 @@ namespace Frog.Domain.IntegrationTests.RabbitMQ
         }
 
         [Test]
-		public void should_send_mesage_and_receive_it(){
+		public void should_send_message_and_receive_it(){
 			var msg = "";
-			bus1.RegisterHandler<MyEvent>(ev =>  msg = ev.Data, "unit_test0");
-			bus1.Publish(new MyEvent{Data = "unit_test1"});
-            AssertionHelpers.WithRetries(() => Assert.That(msg, Is.EqualTo("unit_test1")));
-            bus1.UnregisterHandler<MyEvent>("unit_test0");
+			bus1.RegisterHandler<MyEvent>(ev =>  msg = ev.Data, unitTest0);
+			bus1.Publish(new MyEvent{Data = unitTest1});
+            AssertionHelpers.WithRetries(() => Assert.That(msg, Is.EqualTo(unitTest1)));
+            bus1.UnregisterHandler<MyEvent>(unitTest0);
         }
 
         [Test]
-		public void should_send_mesage_and_all_subscribers_should_receive_it(){
+		public void should_send_message_and_all_queues_should_receive_it(){
 			var msg1 = "";
 			var msg2 = "";
-			bus1.RegisterHandler<MyEvent>(ev =>  msg1 = ev.Data, "unit_test1");
-			bus1.RegisterHandler<MyEvent>(ev =>  msg2 = ev.Data, "unit_test2");
-			bus1.Publish(new MyEvent{Data = "unit_test2"});
-            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo("unit_test2")));
-            AssertionHelpers.WithRetries(() => Assert.That(msg2, Is.EqualTo("unit_test2")));
-            bus1.UnregisterHandler<MyEvent>("unit_test1");
-            bus1.UnregisterHandler<MyEvent>("unit_test2");
+			bus1.RegisterHandler<MyEvent>(ev =>  msg1 = ev.Data, unitTest1);
+			bus1.RegisterHandler<MyEvent>(ev =>  msg2 = ev.Data, unitTest2);
+			bus1.Publish(new MyEvent{Data = unitTest2});
+            AssertionHelpers.WithRetries(() => Assert.That(msg1, Is.EqualTo(unitTest2)));
+            AssertionHelpers.WithRetries(() => Assert.That(msg2, Is.EqualTo(unitTest2)));
+            bus1.UnregisterHandler<MyEvent>(unitTest1);
+            bus1.UnregisterHandler<MyEvent>(unitTest2);
         }
 
         [Test]
 		public void should_send_mesage_and_only_one_subscriber_of_the_queue_will_receive_it(){
 			var msg1 = "";
 			var msg2 = "";
-			bus1.RegisterHandler<MyEvent>(ev =>  msg1 = ev.Data, "unit_test3");
-			bus2.RegisterHandler<MyEvent>(ev =>  msg2 = ev.Data, "unit_test3");
-            bus1.Publish(new MyEvent { Data = "unit_test3" });
+			bus1.RegisterHandler<MyEvent>(ev =>  msg1 = ev.Data, unitTest3);
+			bus2.RegisterHandler<MyEvent>(ev =>  msg2 = ev.Data, unitTest3);
+            bus1.Publish(new MyEvent { Data = unitTest3 });
             Thread.Sleep(3000);
-            AssertionHelpers.WithRetries(() => Assert.That(msg1 + msg2, Is.EqualTo("unit_test3")));
-            bus1.UnregisterHandler<MyEvent>("unit_test3");
+            AssertionHelpers.WithRetries(() => Assert.That(msg1 + msg2, Is.EqualTo(unitTest3)));
+            bus1.UnregisterHandler<MyEvent>(unitTest3);
+            bus2.UnregisterHandler<MyEvent>(unitTest3);
         }
 
 	    [Test]
@@ -95,14 +104,14 @@ namespace Frog.Domain.IntegrationTests.RabbitMQ
 	                                                  }
 	                                                  else msg1.Add(ev.Data);
                                                   else msg1.Add(ev.Data);
-	                                          }, "unit_test4");
+	                                          }, unitTest4);
 	        bus1.Publish(new MyEvent {Data = "unit_test4_1"});
 	        bus1.Publish(new MyEvent {Data = "unit_test4_2"});
 	        AssertionHelpers.WithRetries(() => Assert.That(msg1.Count, Is.EqualTo(2)));
 	        Assert.That(msg1[0], Is.EqualTo("unit_test4_1"));
 	        Assert.That(msg1[1], Is.EqualTo("unit_test4_2"));
 	        Assert.That(exceptions, Is.EqualTo(0));
-	        bus1.UnregisterHandler<MyEvent>("unit_test4");
+	        bus1.UnregisterHandler<MyEvent>(unitTest4);
 	    }
 
         [Test]
@@ -121,15 +130,15 @@ namespace Frog.Domain.IntegrationTests.RabbitMQ
                                      }
                                      Interlocked.Increment(ref messageAlreadyHandled);
                                  };
-            bus1.RegisterHandler<MyEvent>(@event => handler(), "unit_test5");
-            bus1.RegisterHandler<YourEvent>(@event => handler(), "unit_test5");
+            bus1.RegisterHandler<MyEvent>(@event => handler(), unitTest5);
+            bus1.RegisterHandler<YourEvent>(@event => handler(), unitTest5);
             bus1.Publish(new MyEvent { Data = "unit_test5_1" });
             Thread.Sleep(50);
             bus1.Publish(new YourEvent { Data = "unit_test5_2" });
             AssertionHelpers.WithRetries(() => Assert.That(messageAlreadyHandled, Is.EqualTo(2)));
             assertOtherMessageHasNotBeenHandledAlready();
-            bus1.UnregisterHandler<MyEvent>("unit_test5");
-            bus1.UnregisterHandler<YourEvent>("unit_test5");
+            bus1.UnregisterHandler<MyEvent>(unitTest5);
+            bus1.UnregisterHandler<YourEvent>(unitTest5);
         }
 
         [Test]
@@ -141,16 +150,18 @@ namespace Frog.Domain.IntegrationTests.RabbitMQ
                                               {
                                                   Thread.Sleep(1000);
                                                   handler1++;
-                                              }, "unit_test7");
+                                              }, unitTest7);
             bus2.RegisterHandler<MyEvent>(@event =>
                                               {
                                                   Thread.Sleep(1000);
                                                   handler2++;
-                                              }, "unit_test7");
+                                              }, unitTest7);
             bus1.Publish(new MyEvent { Data = "" });
             bus1.Publish(new MyEvent { Data = "" });
             AssertionHelpers.WithRetries(() => Assert.That(handler1, Is.EqualTo(1)));
             AssertionHelpers.WithRetries(() => Assert.That(handler2, Is.EqualTo(1)));
+            bus1.UnregisterHandler<MyEvent>(unitTest7);
+            bus2.UnregisterHandler<MyEvent>(unitTest7);
         }
 	}
 }
