53cc8cc8fcdb26caebce1c209cfe403c22173b62
Petar Shomov <pshomov@gmail.com>
2013-05-04 18:55:07 +0000

    making the agent system test a bit better

--- src/tests/Frog.System.Specs/Agent/AgentComingOnline.cs
+++ src/tests/Frog.System.Specs/Agent/AgentComingOnline.cs
@@ -11,17 +11,20 @@ namespace Frog.System.Specs.ProjectBuilding
     [TestFixture]
     public class AgentComingOnline : SystemBDD
     {
+        private Guid agentId;
+
         protected override void When()
         {
             var sourceRepoDriver = Substitute.For<SourceRepoDriver>();
             var workingAreaGoverner = Substitute.For<WorkingAreaGoverner>();
-            testSystem.AddAgent(url => sourceRepoDriver, workingAreaGoverner, Guid.NewGuid(), "tag1", "tag2");
+            agentId = Guid.NewGuid();
+            testSystem.AddAgent(url => sourceRepoDriver, workingAreaGoverner, agentId, "tag1", "tag2");
         }
 
         [Test]
         public void should_announce_the_project_has_been_checked_out()
         {
-            Assert.True(EventStoreCheck(ES => ES.Has(An.Event<AgentJoined>())));
+            Assert.True(EventStoreCheck(ES => ES.Has(An.Event<AgentJoined>(joined => joined.AgentId == agentId))));
         }
 
         [Test]
